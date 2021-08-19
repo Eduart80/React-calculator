72a059b04641da29bb1df71a68cfeea36de38c3e
edd <zeedymarina@gmail.com>
2021-08-19 01:26:35 -0400

    1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Layout from "./Components/layout";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Layout />
+      </header>
+    </div>
+  );
+}
+
+export default App;
